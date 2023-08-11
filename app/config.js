c3f267127504a74ce8e82921011c966532204edd
John Watson <john.watson1@defra.gov.uk>
2023-08-11 13:58:02 +0100

    Extend cookie

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@ module.exports = {
   port: process.env.PORT || 3011,
   cookie: {
     name: process.env.COOKIE_NAME || 'asbtc-session',
-    maxAge: process.env.COOKIE_DURATION || 28 * 24 * 60 * 60 * 1000, // 28 days in millis
+    maxAge: process.env.COOKIE_DURATION || 60 * 60 * 24 * 365 * 1000, // 1 year
     secret: process.env.COOKIE_SECRET || 'asbtc-secret'
   }
 }
